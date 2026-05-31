7283462a22e9f5cb977199ed3911a1930ce3dc97
Ramz04 <55752850+ramz04@users.noreply.github.com>
2026-05-31 22:16:11 +0000

    Add classic book texts; update main and stats
    Add Project Gutenberg texts for Moby Dick and Pride and Prejudice under books/. Update main.py and stats.py to integrate and handle the new book files (loading/parsing and statistics processing adjustments).

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from stats import count_num_of_words, character_count
+import sys
+from stats import count_num_of_words, character_count, sorted_char_count_dict
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
 
 def get_book_text(path):
-    with open(path) as f:
-        file_contents = f.read()
-        return file_contents
+    with open(path, encoding="utf-8") as f:
+        return f.read()
 
-def main():
-    print("Reading book...")
-    book_text = get_book_text("./books/frankenstein.txt")
-    print(book_text)
-
-# main()
-count_num_of_words(get_book_text)
-print(character_count(get_book_text))
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {book_path}...")
+print("----------- Word Count ----------")
+print(f"Found {count_num_of_words(get_book_text, book_path)} total words")
+print("--------- Character Count -------")
+for char_count in sorted_char_count_dict(character_count(get_book_text, book_path)):
+    if char_count["character"].isalpha():
+        print(f"{char_count['character']}: {char_count['count']}")
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,28 +1,27 @@
 from typing import TypedDict
 
-def count_num_of_words(get_book_text):
-    print("Counting number of words...")
-    book_text = get_book_text("./books/frankenstein.txt")
-    words = book_text.split()
-    nums_of_words = len(words)
-    print(f"Found {nums_of_words} total words")
+def count_num_of_words(get_book_text, path):
+    book_text = get_book_text(path)
+    return len(book_text.split())
 
-
-def character_count(get_book_text):
-    print("Counting number of characters...")
-    book_text = get_book_text("./books/frankenstein.txt")
+def character_count(get_book_text, path):
+    book_text = get_book_text(path)
     char_count = {}
     for char in book_text.lower():
         if char in char_count:
             char_count[char] += 1
         else:
             char_count[char] = 1
-    
     return char_count
 
 class CharacterCount(TypedDict):
     character: str
     count: int
 
-def sorted_char_count_dict(char_count:dict) -> list[CharacterCount]:
-    
+def get_count(char_count: CharacterCount) -> int:
+    return char_count["count"]
+
+def sorted_char_count_dict(char_count: dict) -> list[CharacterCount]:
+    char_count_list = [{"character": k, "count": v} for k, v in char_count.items()]
+    char_count_list.sort(key=get_count, reverse=True)
+    return char_count_list
